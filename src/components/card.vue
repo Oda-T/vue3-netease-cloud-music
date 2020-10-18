026b13c344b4b82ce125067c688979e212dceeb5
Oda-T <46777459+Oda-T@users.noreply.github.com>
2020-10-19 00:31:08 +0800

    router guards

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <!-- 卡片的媒体内容，可以包含图片、视频等媒体内容，以及标题、副标题 -->
-    <div class="mdui-card-media">
+    <div class="mdui-card-media" @mouseenter="toggleCardMenuShow" @mouseleave="toggleCardMenuHide">
       <router-link :to="item.id">
         <img :src="item.picUrl" />
       </router-link>
-      <div class="mdui-card-menu">
-        <button class="mdui-btn mdui-btn-icon mdui-text-color-white  mdui-text-color-theme-700"><i class="mdui-icon material-icons">play_circle_outline</i></button>
+      <div class="c-card-menu-vert mdui-card-menu" :style="{ display: cCardMenuDisplay, opacity: cCardMenuOpacity }">
+        <button class="mdui-btn mdui-btn-icon mdui-text-color-white c-card-menu-vert-btn"><i class="mdui-icon material-icons">more_vert</i></button>
+      </div>
+      <div class="c-card-menu-play" :style="{ display: cCardMenuDisplay, opacity: cCardMenuOpacity }">
+        <button class="mdui-btn mdui-btn-icon mdui-text-color-white c-card-menu-play-btn"><i class="mdui-icon material-icons">play_arrow</i></button>
       </div>
     </div>
 
@@ -22,7 +25,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'Card',
@@ -30,12 +33,58 @@ export default defineComponent({
     item: Object
   },
   setup() {
-    return {}
+    const cCardMenuDisplay = ref('none')
+    const cCardMenuOpacity = ref('0')
+
+    let timer: number
+
+    const toggleCardMenuShow: () => void = () => {
+      clearTimeout(timer)
+
+      cCardMenuDisplay.value = 'block'
+      setTimeout(() => {
+        cCardMenuOpacity.value = '1'
+      }, 10)
+    }
+
+    const toggleCardMenuHide: () => void = () => {
+      cCardMenuOpacity.value = '0'
+      timer = setTimeout(() => {
+        cCardMenuDisplay.value = 'none'
+      }, 500)
+    }
+
+    return {
+      cCardMenuDisplay,
+      cCardMenuOpacity,
+      toggleCardMenuShow,
+      toggleCardMenuHide
+    }
   }
 })
 </script>
 
 <style lang="less" scoped>
+.c-card-menu-vert {
+  transition: opacity 0.5s;
+  display: none;
+  opacity: 0;
+  .c-card-menu-vert-btn {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+}
+.c-card-menu-play {
+  position: absolute;
+  bottom: 16px;
+  right: 16px;
+  z-index: 1;
+  transition: opacity 0.5s;
+  display: none;
+  opacity: 0;
+  .c-card-menu-play-btn {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+}
 .recommend-card-list-title {
   font-size: 16px;
   line-height: 28px;
